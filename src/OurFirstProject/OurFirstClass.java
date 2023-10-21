e8794c4c4f7f0ed7a95af1b196d68ac679f86ba0
OlgaWhiteBelyakova <olga.white.belyakova@gmail.com>
2023-10-21 11:28:42 -0600

    second commit

--- src/OurFirstProject/OurFirstClass.java
+++ src/OurFirstProject/OurFirstClass.java
@@ -5,6 +5,7 @@ public class OurFirstClass {
 	public static void main(String[] args) {
 		
 System.out.println("Hi Emilia!!!");
+System.out.println("looks like i was able to push the changes!");
 		
 	
 
